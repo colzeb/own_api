6f389406cc3e8e76a34c3424c597f63ea0213e38
pankaj <hi.p@outlook.com>
2022-11-06 13:42:53 +0530

    added GET /pet/1

--- src/model/pet.rs
+++ src/model/pet.rs
@@ -1,5 +1,7 @@
-use super::{category::Category, status::Status, tag::Tag};
+use serde::Serialize;
 
+use super::{category::Category, status::Status, tag::Tag};
+#[derive(Debug, Serialize)]
 pub struct Pet {
     pub id: u32,
     pub category: Category,
